88b3da513b0cb219511e127828c3377690312cd1
Marie-Demeter <mdemeter.ducrot@gmail.com>
2022-04-08 16:32:57 +0200

    Feature: Add new image to the gallery #7

--- scripts/galerie.js
+++ scripts/galerie.js
@@ -1,3 +1,4 @@
+const liensImgGalerie = ["https://images.pexels.com/photos/5055188/pexels-photo-5055188.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055189/pexels-photo-5055189.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054687/pexels-photo-5054687.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054688/pexels-photo-5054688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054686/pexels-photo-5054686.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055187/pexels-photo-5055187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055179/pexels-photo-5055179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055182/pexels-photo-5055182.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054690/pexels-photo-5054690.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"];
 
 // Création menu déroulant
 let menuButton = document.createElement("button");
@@ -22,38 +23,34 @@ for (let i = 0; i < liens.length; i++) {
 
 menuButton.onclick = printMenuElements;
 
-// Affichage et création de la galerie
-const liensImgGalerie = ["https://images.pexels.com/photos/5055188/pexels-photo-5055188.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055189/pexels-photo-5055189.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054687/pexels-photo-5054687.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054688/pexels-photo-5054688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054686/pexels-photo-5054686.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055187/pexels-photo-5055187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055179/pexels-photo-5055179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5055182/pexels-photo-5055182.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/5054690/pexels-photo-5054690.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"];
 
-for (let i = 0; i < liensImgGalerie.length; i++) {
-    let galerie = document.querySelector("#galerie");
-    let img = document.createElement("img");
-    img.className = "imgGalerie";
-    img.setAttribute("id", "img" + i);
-    img.src = liensImgGalerie[i];
-    galerie.append(img);
-}
-
-const imagesGalerie = document.querySelectorAll(".imgGalerie");
-for (let i = 0; i < imagesGalerie.length; i++) {
-    imagesGalerie[i].addEventListener("click", showBigImg);
-}
 function showBigImg(event) {
     const imagesGalerie = document.querySelector(".show");
     let idClique = event.target.id;
     let imgAAfficher = document.getElementById(idClique);
     imgAAfficher.classList.toggle("show");
-    if(imagesGalerie || imagesGalerie==imgAAfficher){
-    imagesGalerie.classList.remove("show");
+    if (imagesGalerie || imagesGalerie == imgAAfficher) {
+        imagesGalerie.classList.remove("show");
     }
 }
 
 //Création du bouton changeView
-let button = document.createElement("input");
-document.getElementById("changeView").append(button);
-button.setAttribute('type', 'button');
-button.setAttribute('value', 'Changer la vue de la galerie');
-button.onclick = changeView;
+let buttonChangeView = document.createElement("input");
+buttonChangeView.setAttribute('type', 'button');
+buttonChangeView.setAttribute('value', 'Changer la vue de la galerie');
+document.getElementById("changeView").append(buttonChangeView);
+buttonChangeView.onclick = changeView;
+
+//Création du bouton addImage
+let buttonFormImage = document.createElement("input");
+buttonFormImage.setAttribute('type', 'button');
+buttonFormImage.setAttribute('value', 'Formulaire ajout d\'une image à la galerie');
+document.getElementById("addImage").append(buttonFormImage);
+buttonFormImage.onclick = printFormAddImage;
+
+
+
+
 
 // Ferme le menu déroulant si on clique à coté du menu
 window.onclick = function (event) {
@@ -76,8 +73,54 @@ function printMenuElements() {
 
 
 // Fonction changement de vue galerie
-function changeView(){
+function changeView() {
     const galerie = document.querySelector(".galerie");
     galerie.classList.toggle("changeView")
 
 }
+
+//Fonction printFormAddImage
+function printFormAddImage() {
+    let form = document.createElement("form");
+    let inputURL = document.createElement("input");
+    let buttonAddImage = document.createElement("input");
+
+    inputURL.setAttribute('type', 'text');
+    inputURL.setAttribute('id', 'URL');
+    buttonAddImage.setAttribute('type', 'button');
+    buttonAddImage.setAttribute('value', 'Ajouter');
+    document.getElementById("addImage").append(inputURL);
+    document.getElementById("addImage").append(buttonAddImage);
+    document.getElementById("addImage").append(form);
+
+    buttonAddImage.onclick = addImage;
+}
+
+//Fonction printFormAddImage
+function addImage() {
+    const newURL = document.getElementById("URL").value;
+    console.log(newURL)
+    liensImgGalerie.push(newURL);
+    printGallery(liensImgGalerie)
+}
+
+// Fonction affichage et création de la galerie
+function printGallery(liensImgGalerie) {
+    for (let i = 0; i < liensImgGalerie.length; i++) {
+        let galerie = document.querySelector("#galerie");
+        let img = document.createElement("img");
+        img.className = "imgGalerie";
+        img.setAttribute("id", "img" + i);
+        img.src = liensImgGalerie[i];
+
+
+        galerie.append(img);
+
+        const imagesGalerie = document.querySelectorAll(".imgGalerie");
+        for (let i = 0; i < imagesGalerie.length; i++) {
+            imagesGalerie[i].addEventListener("click", showBigImg);
+        }
+    }
+}
+
+printGallery(liensImgGalerie)
